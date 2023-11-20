3d3ccbc3e9696c62a6c415ac09c7026166509d18
suinww <a20307@naver.com>
2023-11-20 16:27:10 +0900

    commit

---
+++ app/src/main/java/com/example/mygallery/MainActivity.kt
@@ -0,0 +1,150 @@
+package com.example.mygallery
+
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.mygallery.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private val imageLoadLauncher =
+        registerForActivityResult(ActivityResultContracts.GetMultipleContents()) { uriList ->
+            updateImages(uriList)
+        }
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var imageAdapter: ImageAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.toolbar.apply{
+            title = "사진 가져오기"
+            setSupportActionBar(this)
+        }
+
+        binding.loadImageBtn.setOnClickListener {
+            checkPermission()
+        }
+        binding.frameActivityBtn.setOnClickListener {
+            navigateToFrameActivity()
+        }
+        initRecyclerView()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_add->{
+                checkPermission()
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+    }
+    private fun navigateToFrameActivity() {
+        val images = imageAdapter.currentList.filterIsInstance<ImageItems.Image>()
+            .map { it.uri.toString() }.toTypedArray()
+        val intent = Intent(this, FrameActivity::class.java)
+            .putExtra("images", images)
+        startActivity(intent)
+    }
+
+    private fun initRecyclerView() {
+        imageAdapter = ImageAdapter(object : ImageAdapter.ItemClickListener {
+            override fun onLoadMoreClick() {
+                checkPermission()
+            }
+        })
+        binding.imageRecyclerView.apply {
+            adapter = imageAdapter
+            layoutManager = GridLayoutManager(context, 2)
+        }
+    }
+
+    private fun checkPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this, android.Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                loadImage()
+                //권한이 있으면 이미지 가져오기
+            }
+            shouldShowRequestPermissionRationale(
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            ) -> {
+                showPermissioninfoDialog()
+                // 권한이 허락되지않았으면 권한요청다이얼로그 띄우기
+            }
+            else -> {
+                requestReadExternalStorage()
+            }
+        }
+    }
+
+    private fun showPermissioninfoDialog() {
+        AlertDialog.Builder(this).apply {
+            setMessage("이미지를 가져오기 위해서 외부저장소 읽기 권한이 필요합니다.")
+            setNegativeButton("취소", null)
+            setPositiveButton("동의") { _, _ ->
+                requestReadExternalStorage()
+            }.show()
+        }
+    }
+
+    private fun requestReadExternalStorage() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+            REQUEST_READ_EXTERNAL_STORAGE
+        )
+    }
+
+    private fun loadImage() {
+        imageLoadLauncher.launch("image/*")
+    }
+
+    private fun updateImages(uriList: List<Uri>) {
+        Log.i("updateImages", "$uriList")
+        val images = uriList.map { ImageItems.Image(it) }
+        val updatedImages = imageAdapter.currentList.toMutableList().apply { addAll(images) }
+        imageAdapter.submitList(updatedImages)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_READ_EXTERNAL_STORAGE -> {
+                val resultCode = grantResults.firstOrNull() ?: PackageManager.PERMISSION_GRANTED
+                if (resultCode == PackageManager.PERMISSION_GRANTED) {
+                    loadImage()
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val REQUEST_READ_EXTERNAL_STORAGE = 100
+    }
+}
